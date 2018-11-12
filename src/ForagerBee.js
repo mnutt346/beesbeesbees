1cca2b493267d172d4f14b7982d567314151434e
Michael Nutt <mnutt346@gmail.com>
2018-11-12 12:19:59 -0600

    Passed all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
 };
+
+ForagerBee.prototype = new Bee();
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage(treasure) {
+    this.treasureChest.push(treasure);
+}
 
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,15 @@
 var RetiredForagerBee = function() {
+    ForagerBee.call(this);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = new ForagerBee();
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage() {
+    return 'I am too old, let me play cards instead';
+}
